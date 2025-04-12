dafdfd0c0bb63742f90f0a7c90f555f1a22f26bf
ys7i <s7i.yuhi@gmail.com>
2025-04-12 10:18:12 +0900

    Initial commit

---
+++ lib/yfinance_wrapper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "yfinance_wrapper/version"
+
+module YfinanceWrapper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/yfinance_wrapper/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module YfinanceWrapper
+  VERSION = "0.1.0"
+end
