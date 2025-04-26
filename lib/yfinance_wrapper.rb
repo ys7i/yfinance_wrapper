3ecd187e7dcc163256cd9eafe5f2afd005471443
ys7i <s7i.yuhi@gmail.com>
2025-04-26 16:31:56 +0900

    Fix rubocop error

--- lib/yfinance_wrapper.rb
+++ lib/yfinance_wrapper.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 require_relative "yfinance_wrapper/ticker"
+# rubocop:disable Style/MixinUsage
 include PyCall::Import
+# rubocop:enable Style/MixinUsage
+
 module YfinanceWrapper
   pyimport :yfinance, as: :yf
   # rubocop:disable Style/ClassVars
